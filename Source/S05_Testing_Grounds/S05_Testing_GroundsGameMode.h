2ad339ba2f944c6022d76e75c31329fa562d3419
PATRICK-PC\Patrick <patrick68794@gmail.com>
2018-02-21 19:40:28 -0600

    Initial commit

---
+++ Source/S05_Testing_Grounds/S05_Testing_GroundsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "S05_Testing_GroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AS05_Testing_GroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AS05_Testing_GroundsGameMode();
+};
+
+
+
