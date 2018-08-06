1f5daf4920cd5c52f8839e02531ac715280255d1
PATRICK-PC\Patrick <patrick68794@gmail.com>
2018-08-05 20:40:00 -0500

    TG42 Refactoring to a Gun Actor

---
+++ Source/S05_Testing_Grounds/Weapons/Gun.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class S05_TESTING_GROUNDS_API AGun : public AActor
+{
+	GENERATED_BODY()
+
+		/** Gun mesh: 1st person view (seen only by self) */
+		UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* FP_MuzzleLocation;
+	
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+		TSubclassOf<class ABallProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class UAnimMontage* FireAnimation;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** Fires a projectile. */
+	void OnFire();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimInstance* AnimInstance;
+	
+};
