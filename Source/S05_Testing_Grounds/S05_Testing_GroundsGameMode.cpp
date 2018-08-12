5f22e4790bc70c870d939ed2a0f83d2def57fb87
PATRICK-PC\Patrick <patrick68794@gmail.com>
2018-08-12 15:04:22 -0400

    TG46 Child Actor Components

--- Source/S05_Testing_Grounds/S05_Testing_GroundsGameMode.cpp
+++ Source/S05_Testing_Grounds/S05_Testing_GroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05_Testing_GroundsGameMode::AS05_Testing_GroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
